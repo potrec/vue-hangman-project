397563327ecb1c78aa5a7b6cae7b7c1712079609
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-05-21 14:15:35 +0200

    Add word component

---
+++ vue-hangman-project/src/components/Word.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="word">
+        <span v-for="(letter, i) in wrongLetters" :key="letter">
+            {{ correctLetters.includes(letter) ? letter : ''}}
+        </span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        letters: { type: Array, default: () => []}
+        correctLetters: { type: Array, default: () => []}
+    }
+}
+</script>
