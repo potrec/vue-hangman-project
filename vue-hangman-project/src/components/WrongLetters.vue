c37f2fd23a515f52224d77a71b7ad9cc17b50b75
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-05-21 14:15:49 +0200

    Add WrongLetters component

---
+++ vue-hangman-project/src/components/WrongLetters.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="">
+        <p v-show="wrongLetters.length > 0"></p>
+        <span v-for="(letter, i) in wrongLetters" :key="letter">
+            {{ letter }}
+            {{ i === wrongLetters.length - 1 ? '' : ', '}}
+        </span>
+    </div>
+
+</template>
+
+<script>
+export default {
+    props: {
+        wrongLetters: { type: Array, default: () => []}
+    }
+}
+</script>
