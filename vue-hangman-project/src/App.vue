c3df0f933df2b932670f9a262625d872e576938a
Maciej Potręć <73169698+potrec@users.noreply.github.com>
2024-05-21 14:15:59 +0200

    Clean up

--- vue-hangman-project/src/App.vue
+++ vue-hangman-project/src/App.vue
@@ -1,7 +1,11 @@
 <script>
 import Figure from './components/Figure.vue';
+import Header from './components/Header.vue';
 </script>
 <template>
-
+  <div>test
+    <Header/>
+    <Figure/>
+  </div>
 </template>
 
